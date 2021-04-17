f1b44fa5050e418ddf942b0ffa48e1ef794ad131
HeadBangZ <thomasnhprivat@gmail.com>
2021-04-17 20:50:38 +0200

    Send data to reservation summary and redirect if no data

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/HeadBangZ/bookings/internal/config"
 	"github.com/HeadBangZ/bookings/internal/handlers"
+	"github.com/HeadBangZ/bookings/internal/models"
 	"github.com/HeadBangZ/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,9 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	// What is being placed in session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,8 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/about", handlers.Repo.About)
 
+	mux.Get("/contact", handlers.Repo.Contact)
+
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 
 	mux.Get("/majors-suite", handlers.Repo.Majors)
@@ -28,9 +30,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
-	mux.Get("/contact", handlers.Repo.Contact)
-
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
+	mux.Post("/make-reservation", handlers.Repo.PostReservation)
+
+	mux.Get("/reservation-summary", handlers.Repo.ReservationSummary)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
