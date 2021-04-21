90e81c99f2cb4b6c886fb25478b17f80ff1690e9
HeadBangZ <thomasnhprivat@gmail.com>
2021-04-21 19:38:06 +0200

    Added db connection and models and refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/HeadBangZ/bookings/internal/config"
+	"github.com/HeadBangZ/bookings/internal/driver"
 	"github.com/HeadBangZ/bookings/internal/handlers"
 	"github.com/alexedwards/scs/v2"
 
@@ -27,10 +28,12 @@ var errorLog *log.Logger
 
 // main is the main function
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer db.SQL.Close()
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 
@@ -45,9 +48,12 @@ func main() {
 	}
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
+	gob.Register(models.User{})
+	gob.Register(models.Room{})
+	gob.Register(models.Restriction{})
 
 	// change this to true when in production
 	app.InProduction = false
@@ -67,19 +73,27 @@ func run() error {
 
 	app.Session = session
 
+	// connect to database
+	log.Println("Connection to database...")
+	db, err := driver.ConnectSQL("")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	log.Println("Connected to database")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = false
 
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
-	render.NewTemplates(&app)
+	render.NewRenderer(&app)
 	helpers.NewHelpers(&app)
 
-	return nil
+	return db, nil
 }
